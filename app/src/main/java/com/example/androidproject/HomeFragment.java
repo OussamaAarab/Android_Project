2297ece0115bb2132d2813857dc9a2f17a5b8992
Oussama Aarab <oussamaaarab5712@gmail.com>
2021-04-13 02:16:17 +0000

    Movies Slider

--- app/src/main/java/com/example/androidproject/HomeFragment.java
+++ app/src/main/java/com/example/androidproject/HomeFragment.java
@@ -11,15 +11,22 @@ import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.viewpager.widget.ViewPager;
 
 import com.example.androidproject.Handlers.MovieHandler;
 import com.example.androidproject.HomeAdapter.AdapterMovies;
+import com.example.androidproject.HomeAdapter.Slide;
+import com.example.androidproject.HomeAdapter.SlideAdapter;
 import com.example.api.API_Factory;
 import com.example.api.API_Movie;
 import com.example.beans.Movie;
+import com.google.android.material.tabs.TabLayout;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.List;
+import java.util.Timer;
+import java.util.TimerTask;
 
 public class HomeFragment extends Fragment {
     View v ;
@@ -39,6 +46,10 @@ public class HomeFragment extends Fragment {
     ArrayList<Movie> movies = new ArrayList<>();
     ArrayList<Movie> series = new ArrayList<>();
 
+    private List<Slide> liste_Slide = new ArrayList<>();
+    private ViewPager sliderpager;
+    private TabLayout indicator;
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
@@ -52,6 +63,19 @@ public class HomeFragment extends Fragment {
 
         handlerMovie = new MovieHandler();
         handlerSeries = new MovieHandler();
+
+        sliderpager = v.findViewById(R.id.auto_slide);
+        indicator = v.findViewById(R.id.indicator);
+        liste_Slide.add(new Slide(R.drawable.backdrop1,"Mission Impossible"));
+        liste_Slide.add(new Slide(R.drawable.backdrop2,"Final Destination"));
+        liste_Slide.add(new Slide(R.drawable.backdrop3,"Harry Potter"));
+        liste_Slide.add(new Slide(R.drawable.backdrop4,"Avengers"));
+        liste_Slide.add(new Slide(R.drawable.backdrop5,"Rush Hour"));
+        SlideAdapter slideAdapter = new SlideAdapter(getActivity(), liste_Slide);
+        sliderpager.setAdapter(slideAdapter);
+        Timer timer = new Timer();
+        timer.scheduleAtFixedRate(new HomeFragment.SliderTimer(), 4000, 6000);
+        indicator.setupWithViewPager(sliderpager,true);
 
         new Thread(new Runnable() {
             @Override
@@ -110,5 +134,25 @@ public class HomeFragment extends Fragment {
         adapters = new AdapterMovies(movies);
         recycler.setAdapter(adapters);
         return  adapters;
+    }
+
+    class SliderTimer extends TimerTask{
+
+        @Override
+        public void run() {
+            getActivity().runOnUiThread(new Runnable() {
+                @Override
+                public void run() {
+                    if(sliderpager.getCurrentItem() < liste_Slide.size()-1)
+                    {
+                        sliderpager.setCurrentItem(sliderpager.getCurrentItem()+1);
+                    }
+                    else
+                    {
+                        sliderpager.setCurrentItem(0);
+                    }
+                }
+            });
+        }
     }
 }
