7566751df872f1121a1adf1feb65481af3efb542
Abderrahim <abderrahimdeaabane@gmail.com>
2021-05-31 16:50:36 +0100

    -- sync push --

--- app/src/main/java/com/example/androidproject/Handlers/MovieHandler.java
+++ app/src/main/java/com/example/androidproject/Handlers/MovieHandler.java
@@ -36,7 +36,8 @@ public class MovieHandler extends Handler {
     @Override
     public void handleMessage(@NonNull Message msg) {
         super.handleMessage(msg);
-        if (msg.arg1 == HomeFragment.MSG_START) {
+        if (msg.arg1 == HomeFragment.MSG_START)
+        {
             HashMap<String, Object> objMovie = (HashMap<String, Object>) msg.obj;
             ArrayList<Movie> movies = (ArrayList<Movie>) objMovie.get("MoviesList");
             AdapterMovies adapter = (AdapterMovies) objMovie.get("AdapterMovies");
